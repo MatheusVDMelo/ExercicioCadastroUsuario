94bcdd45e9b3a7f317d2caf86cda0568564729b2
dieisson <dieisson.qfloriano@gmail.com>
2026-04-16 20:51:53 -0300

    cadastrar usuario novo

--- ExercicioCadastroUsuario/Program.cs
+++ ExercicioCadastroUsuario/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string[] nome = new string[10];
+string[] senha = new string[10];
+
+
+for (int i = 0; i < nome.Length; i++)
+{
+    Console.WriteLine("crie um usuario:");
+    nome[i] = Console.ReadLine();
+
+}
