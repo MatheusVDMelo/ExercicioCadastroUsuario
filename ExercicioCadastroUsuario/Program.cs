9eab10429202096e92b2b41e41b4b9183e1bae14
Matheus Melo <matheus.vitordemelo@gmail.com>
2026-04-16 21:22:15 -0300

    listar usuários

--- ExercicioCadastroUsuario/Program.cs
+++ ExercicioCadastroUsuario/Program.cs
@@ -1,7 +1,10 @@
+<<<<<<< Updated upstream
+﻿
+=======
 ﻿string[] nome = new string[10];
-int contador = new int[10];
 
 for (int i = 0; i < nome.Length; i++)
 {
     Console.WriteLine(nome[i]);
 }
+>>>>>>> Stashed changes
