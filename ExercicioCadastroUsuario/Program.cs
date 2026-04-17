eefe83389b79ca101bb152d157c39024abf05423
Alvaro_Santos <alvarotumso14@gmail.com>
2026-04-16 21:04:32 -0300

    sistema_menu_simples

--- ExercicioCadastroUsuario/Program.cs
+++ ExercicioCadastroUsuario/Program.cs
@@ -1,1 +1,28 @@
 ﻿
+int opcao = 0;
+do
+{
+    Console.WriteLine("Menu:");
+    Console.WriteLine("1 - Cadastrar");
+    Console.WriteLine("2 - Listar");
+    Console.WriteLine("3 - Buscar ou remover");
+    Console.Write("Escolha uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine("Cadastro selecionado.");
+            // Lógica para cadastrar
+            break;
+        case 2:
+            Console.WriteLine("Listar selecionado.");
+            // Lógica para listar
+            break;
+        case 3:
+            Console.WriteLine("Buscar e remoção selecionada");
+            break;
+        default:
+            Console.WriteLine("Opção inválida. Tente novamente.");
+            break;
+    }
+} 
