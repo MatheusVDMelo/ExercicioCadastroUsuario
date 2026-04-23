87257e01111f4b0589647dda334fbee261345270
Matheus Melo <matheus.vitordemelo@gmail.com>
2026-04-23 20:26:31 -0300

    ajuste de exibicoes

--- ExercicioCadastroUsuario/Program.cs
+++ ExercicioCadastroUsuario/Program.cs
@@ -1,33 +1,37 @@
 int opcao = 0;
-string[] nomeCadastro = new string[1];
+string[] nomeCadastro = new string[10];
 int indice = 0;
 
 while (opcao != 4)
 {
-    Console.WriteLine("Escolha uma opção");
-    Console.WriteLine("1 - Cadastro");
-    Console.WriteLine("2 - Listagem e exebição");
-    Console.WriteLine("3 - Busca e remoção");
-    Console.WriteLine("4 - Sair");
+    Console.WriteLine(" -------------------------------");
+    Console.WriteLine("|    Escolha uma opção:         |");
+    Console.WriteLine("|    1 - Cadastro;              |");
+    Console.WriteLine("|    2 - Listagem e exebição;   |");
+    Console.WriteLine("|    3 - Busca e remoção;       |");
+    Console.WriteLine("|    4 - Sair.                  |");
+    Console.WriteLine(" ------------------------------- ");
     opcao = int.Parse(Console.ReadLine());
 
     switch (opcao)
     {
         case 1:
-            { 
-                Console.WriteLine("Digite o nome do cadastro");
+            {
+                Console.Clear();
+                Console.Write("Digite o nome do cadastro:");
                 nomeCadastro[indice] = Console.ReadLine();
                 indice++;
             }
             break;
         case 2:
+            Console.Clear();
             for (int i = 0; i < nomeCadastro.Length; i++)
             {
                 Console.WriteLine(nomeCadastro[i]);
             }
             break;
         case 3:
-            Console.WriteLine("Digite o nome do cadastro para remoção");
+            Console.WriteLine("Digite o nome do cadastro para remoção:");
             string nomeRemocao = Console.ReadLine();
             for (int i = 0; i < indice; i++)
             {
@@ -40,7 +44,7 @@ while (opcao != 4)
             }
             break;
         case 4:
-            Console.WriteLine("Saindo do programa...");
+            Console.WriteLine("Programa encerrado com sucesso.");
             break;
 
         default:
